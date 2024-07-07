5a4e8adb7017fa5f165c20c2d35fb61188bf3ca7
Gaurav14 <gauravgandal@gmail.com>
2024-07-07 12:23:24 +0530

    Division by Gaurav

---
+++ division.js
@@ -0,0 +1,11 @@
+// Created by Gaurav
+
+let a = 12;
+let b = 3;
+
+console.log(`Division of ${a} by ${b} is ${a/b}`)
+const res = (x, y) => {
+    console.log(`Division of ${x} by ${y} is ${x/y}`)
+}
+
+res(10, 5)
